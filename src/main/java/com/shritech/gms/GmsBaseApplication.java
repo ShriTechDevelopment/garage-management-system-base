59a04a5a1f2ffe1b058fb6a52379ad479ea3d470
shrikant.pradhan <shreepradhan0807@gmail.com>
2022-01-28 11:24:59 +0530

    First commit

---
+++ src/main/java/com/shritech/gms/GmsBaseApplication.java
@@ -0,0 +1,13 @@
+package com.shritech.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsBaseApplication.class, args);
+	}
+
+}
